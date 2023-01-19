9184351e6d0d9aff8c0deeffa8a2d81c02d7766e
DIR\janani.a.sridhar <janani.a.sridhar@accenture.com>
2023-01-19 14:56:20 +0530

    Changes for list files and tab

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import React, { Component } from "react";
 import "./App.css";
 import Listfiles from "./components/listfiles/listfiles";
 import Uploadfile from "./components/uploadfile/uploadfile";
-
+import Tabs from "react-bootstrap/Tabs";
+import Tab from "react-bootstrap/Tab";
 class App extends Component {
   render() {
     return (
       <>
         <h1 className="title">Pdf Comparator</h1>
-        <Uploadfile></Uploadfile>
-        <Listfiles></Listfiles>
+        <Tabs defaultActiveKey="fileView" id="HomeTab" className="mb-3">
+          <Tab eventKey="fileView" title="View Files">
+            <Listfiles></Listfiles>
+          </Tab>
+          <Tab eventKey="uploadFiles" title="Upload files">
+            <Uploadfile></Uploadfile>
+          </Tab>
+        </Tabs>
       </>
     );
   }
--- src/components/listfiles/listfiles.js
+++ src/components/listfiles/listfiles.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import styles from "./listfiles.module.css";
 import axios from "axios";
+import Table from "react-bootstrap/Table";
+import pdf from "../../images/pdfdownload.png";
 
 function Listfiles() {
   const [stageFilenames, setStageFilenames] = useState([]);
@@ -27,16 +29,86 @@ function Listfiles() {
   React.useEffect(() => listFileIndir(), []);
   return (
     <div className={styles.Listfiles} data-testid="Listfiles">
-      <div>
-        {stageFilenames.map(function (d, idx) {
-          return <li key={idx}>{d}</li>;
-        })}
-        {prodfilenames.map(function (d, idx) {
-          return <li key={idx}>{d}</li>;
-        })}
-        {summaryfilenames.map(function (d, idx) {
-          return <li key={idx}>{d}</li>;
-        })}
+      <div className={styles.stBox} id="stBox">
+        <Table size="sm" responsive>
+          <thead>
+            <tr>
+              <th>Stage Folder Files</th>
+            </tr>
+          </thead>
+          <tbody>
+            {stageFilenames.map(function (d, index) {
+              return (
+                <tr key={index}>
+                  <td>{d}</td>
+                  <td>
+                    {/* <a
+                      href={require("../../java/files/Stage/" + d)}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    > */}
+                    <img
+                      src={pdf}
+                      className={styles.imgicon}
+                      alt="pdffile"
+                    ></img>
+                    {/* </a> */}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+      <div className={styles.ndBox} id="ndBox">
+        <Table size="sm" responsive>
+          <thead>
+            <tr>
+              <th>Prod Folder Files</th>
+            </tr>
+          </thead>
+          <tbody>
+            {prodfilenames.map(function (d, index) {
+              return (
+                <tr key={index}>
+                  <td>{d}</td>
+                  <td>
+                    <img
+                      src={pdf}
+                      className={styles.imgicon}
+                      alt="pdffile"
+                    ></img>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </div>
+      <div className={styles.rdBox} id="rd-box">
+        <Table size="sm" responsive>
+          <thead>
+            <tr>
+              <th>Summary Folder Files</th>
+            </tr>
+          </thead>
+          <tbody>
+            {summaryfilenames.map(function (d, index) {
+              return (
+                <tr key={index}>
+                  <td>{d}</td>
+                  <td>
+                    <img
+                      src={pdf}
+                      className={styles.imgicon}
+                      alt="pdffile"
+                    ></img>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
       </div>
     </div>
   );
