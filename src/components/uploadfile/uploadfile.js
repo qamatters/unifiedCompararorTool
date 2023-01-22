569de45f3c7f8b90b227f6c116d32f9aa9979c87
DIR\janani.a.sridhar <janani.a.sridhar@accenture.com>
2023-01-22 18:22:01 +0530

    Removed comments

--- src/components/uploadfile/uploadfile.js
+++ src/components/uploadfile/uploadfile.js
@@ -47,6 +47,11 @@ const Uploadfile = () => {
         }
       );
       console.log(res);
+      if (path === "Prod") {
+        setShowalert2(true);
+      } else if (path === "Stage") {
+        setShowalert1(true);
+      }
     } catch (err) {
       if (err.response.status === 500) {
         console.log(err);
@@ -56,7 +61,7 @@ const Uploadfile = () => {
     }
   };
 
-  const onFileChange1 = (event) => {
+  /*  const onFileChange1 = (event) => {
     //this.setState({ selectedFile1: event.target.files[0] });
     setSelectedFile1(event.target.files[0]);
   };
@@ -101,7 +106,7 @@ const Uploadfile = () => {
         console.log(err);
       });
   };
-
+ */
   const generateCompareFile = () => {
     console.log("compare call");
     setLoading(true);
@@ -249,118 +254,5 @@ const Uploadfile = () => {
     </>
   );
 };
-/* class Uploadfile extends Component {
-  state = {
-    selectedFile1: null,
-    selectedFile2: null,
-  };
-
-  onFileChange1 = (event) => {
-    this.setState({ selectedFile1: event.target.files[0] });
-  };
-
-  onFileUpload1 = () => {
-    const formData = new FormData();
-    formData.append("myFile1", this.state.selectedFile1);
-
-    console.log(this.state.selectedFile1);
-    axios
-      .post("http://localhost:8080/api/uploadfile1", formData)
-      .then((response) => {
-        console.log("File 1 uploaded");
-
-        console.log(response.data);
-      })
-      .catch((err) => {
-        // Handle error
-        console.log(err);
-      });
-  };
-  onFileChange2 = (event) => {
-    this.setState({ selectedFile2: event.target.files[0] });
-  };
-
-  onFileUpload2 = () => {
-    const formData = new FormData();
-    formData.append("myFile2", this.state.selectedFile2);
-
-    console.log(this.state.selectedFile2);
-    axios
-      .post("http://localhost:8080/api/uploadfile2", formData)
-      .then((response) => {
-        console.log("File 2 uploaded");
-
-        console.log(response.data);
-      })
-      .catch((err) => {
-        // Handle error
-        console.log(err);
-      });
-  };
-
-  generateCompareFile = () => {
-    console.log("compare call");
-    axios
-      .get("http://localhost:8080/api/compare")
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((err) => {
-       
-        console.log(err);
-      });
-  };
-  render() {
-    return (
-      <div className={styles.Uploadfile} data-testid="Uploadfile">
-        <Form>
-          <Form.Text className="text-muted" size="sm">
-            Select File to be uploaded
-          </Form.Text>
-          <Form.Group className="mb-3" controlId="formFileUpload1">
-            <InputGroup>
-             
-              <Form.Control
-                type="file"
-                size="sm"
-                placeholder="Select file"
-                onChange={this.onFileChange1}
-              />
-
-              <Button variant="primary" size="sm" onClick={this.onFileUpload1}>
-                Upload Doc1
-              </Button>
-            </InputGroup>
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formFileUpload2">
-            <InputGroup>
-            
-              <Form.Control
-                type="file"
-                size="sm"
-                placeholder="Select file"
-                onChange={this.onFileChange2}
-              />
-
-              <Button variant="primary" size="sm" onClick={this.onFileUpload2}>
-                Upload Doc2
-              </Button>
-            </InputGroup>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="compare">
-            <Button
-              variant="primary"
-              size="sm"
-              onClick={this.generateCompareFile}
-            >
-              Compare
-            </Button>
-          </Form.Group>
-        </Form>
-      </div>
-    );
-  }
-} */
 
 export default Uploadfile;
